dfb5d58f5db8d196d00094e17598c9a3c59e5b43
Amuriana <pollicevanessa@gmail.com>
2020-03-25 12:09:07 -0400

    fix status next

--- lib/Send_SMS/SMS.rb
+++ lib/Send_SMS/SMS.rb
@@ -6,13 +6,16 @@ module SendSms
     class Sms 
         def initialize
         end
+        
         def send_sms()
             account_sid = ENV['TWILIO_ACCOUNT_SID']
             auth_token = ENV['TWILIO_AUTH_TOKEN']
             client = Twilio::REST::Client.new(account_sid, auth_token)
+            
             from = ENV['TWILIO_PHONE_NUMBER']
             to = '+15146233908'
-            client.message.create(
+            
+            client.messages.create(
                 from: from,
                 to: to,
                 body: "Hello technician. 
