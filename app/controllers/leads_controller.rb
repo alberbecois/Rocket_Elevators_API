2eaea7896c6d5637e9f4cdbc98dbe016c3468bd5
benland24 <benlanduf024@gmail.com>
2020-03-25 17:42:01 -0400

    don't work

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -6,24 +6,21 @@ class LeadsController < ApplicationController
     @lead = Lead.new #generate data blank to create new form
   end
 
-  def new_lead
-    p = params["lead"].permit!
-    puts "PARAMS = #{p}"
-    file_attachment = p["file_attachment"]
-    if file_attachment != nil
-      p["file_attachment"] = file_attachment.read
-      p["original_file_name"] = file_attachment.original_filename
-    end
- 
- 
-    lead = Lead.new(p)
-    lead.valid?
-    p lead.errors
-    lead.save!
-  end
-  
   def create
     @lead = Lead.new(lead_params)
+    puts "CREATING"
+    p = params["lead"].permit!
+    puts "PARAMS = #{p}"
+    file_attachment = p["attached_file"]
+    # file_attachment = params["attached_file"]
+    if file_attachment != nil
+      # p["attached_file"] = file_attachment.read
+      p["original_file_name"] = file_attachment.original_filename
+      puts "WRITING UPLOAD"
+      File.open(Rails.root.join('public', file_attachment.original_filename), 'wb') do |file|
+        file.write(file_attachment.read)
+      end
+    end
     #render json: @lead #test when submit button form
     if @lead.save
       flash[:notice] = "We received your request! "
@@ -33,14 +30,14 @@ class LeadsController < ApplicationController
       redirect_to action:"new"
     end
   end
-
+  
   def edit
   end
   #for get params when click submit form
   
   private
   def lead_params
-    #params.require(name model)
+    # params.require(name model)
     params.require(:lead).permit(:full_name,:company_name,:email,:phone,:project_name,:project_desc,:department,:message,:attached_file)
   end
 end
