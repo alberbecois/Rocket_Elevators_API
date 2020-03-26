542d8b1261afb37382ae70b830ba39ec91dbc8fe
Joshua Knutson <37628377+alberbecois@users.noreply.github.com>
2020-03-26 08:54:49 -0400

    Commit dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,7 +3,6 @@ ActiveAdmin.register_page "Dashboard" do
 
    content title: proc { I18n.t("active_admin.dashboard") } do
 
-<<<<<<< HEAD
     panel "google map" do
       render 'map'
     end
@@ -16,17 +15,6 @@ ActiveAdmin.register_page "Dashboard" do
     panel "elevators per customer" do
     #   column_chart Elevator.group()
     end
-=======
-#     panel "contact requests" do
-#  #     column_chart Lead.group_by_month(:created_at).count
-#     end
-#     panel "bid solicitations" do
-#   #    column_chart Quote.group_by_month(:created_at).count
-#     end
-#     panel "elevators per customer" do
-#     #   column_chart Elevator.group()
-#     end
->>>>>>> 2dc4822effcf09a3e6d877168cd922b638e4b403
 
    end # content
  end
