4dd4d1e01380406d7592388ad4b555d4b37e8b8d
Amuriana <pollicevanessa@gmail.com>
2020-03-26 09:44:15 -0400

    send sms to Antoine Deblonde

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,10 +1,14 @@
 require 'send_sms/sms'
+
 class Elevator < ApplicationRecord
     belongs_to :column
+
     after_save :send_sms
+
     def send_sms
         sms = SendSms::Sms.new
         sms.send_sms
-    end
+    end         
+
 end 
 
