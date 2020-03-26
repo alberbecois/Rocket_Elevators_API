5da872df21e99d24d8574bc97b599152cf5825d6
Kamiwackii <jackie.aki.lai@gmail.com>
2020-03-27 07:43:39 +0900

    put 1 more ENV var, now sending a bit better sms

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,4 +1,4 @@
-require 'send_sms/sms'
+require 'Send_SMS/SMS'
 
 class Elevator < ApplicationRecord
     belongs_to :column
@@ -6,9 +6,10 @@ class Elevator < ApplicationRecord
     after_save :send_sms
 
     def send_sms
-        sms = SendSms::Sms.new
-        sms.send_sms
+        if (self.saved_change_to_status? && self.status == "intervention")
+            sms = SendSms::Sms.new
+            sms.send_sms(self.id, self.column.battery.building.id, self.status)
+        end
     end         
-
 end 
 
