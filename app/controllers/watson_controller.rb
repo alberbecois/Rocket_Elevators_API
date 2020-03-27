3946fa930653479b0a9bf60005065d87b66482b6
Joshua Knutson <37628377+alberbecois@users.noreply.github.com>
2020-03-27 15:28:44 -0400

    Watson done

--- app/controllers/watson_controller.rb
+++ app/controllers/watson_controller.rb
@@ -13,11 +13,19 @@ class WatsonController < ApplicationController
         )
         text_to_speech.service_url = "https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/565cdf45-d8b6-4705-ae30-f9795576293e"
 
-        name = "Dude"
+        name = "#{current_employee.first_name} #{current_employee.last_name}"
+        total_elevators = Elevator.count
+        total_buildings = Building.count
+        total_customers = Customer.count
+        out_of_service = Elevator.where.not(status:'active').count
+        total_quotes = Quote.count
+        total_leads = Lead.count
+        total_batteries = Battery.count
+        total_cities = Address.distinct.count(:city)
 
         File.open("app/assets/audio/welcome.wav", "wb") do |audio|
             response = text_to_speech.synthesize(
-                text: "Welcome, #{name}",
+                text: "Welcome, #{name}. You have #{total_elevators.to_s} elevators deployed in #{total_buildings.to_s} building belonging to #{total_customers.to_s} customers. Currently there are #{out_of_service.to_s} elevators out of service and #{total_quotes.to_s} quotes waiting to be processed. There are also #{total_leads.to_s} leads waiting and #{total_batteries.to_s} batteries running across #{total_cities.to_s} cities",
                 accept: "audio/wav",
                 voice: "en-GB_KateVoice"
             )
